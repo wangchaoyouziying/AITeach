d0c4166d3463a0b7419d02197d82e0b5be296d99
wangchaoyouziying <wangchaozhangshu@163.com>
2022-02-07 22:56:45 +0800

    CMS内容管理

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -1,8 +1,31 @@
 //public是对axios的工具类封装，定义了http请求方法
 import http from './../../../base/api/public'
+import querystring from 'querystring'
 let sysConfig = require('@/../config/sysConfig')
 //解决跨域问题
 let apiUrl = sysConfig.xcApiUrlPre;
 export const page_list = (page, size, params) =>{
-  return http.requestQuickGet(apiUrl+'/cms/page/list/' + page + "/" + size)
+  //将json对象转化为key/value对
+  let query = querystring.stringify(params)
+  return http.requestQuickGet(apiUrl+'/cms/page/list/' + page + "/" + size + "/?" + query)
+}
+
+//页面添加
+export const page_add = params => {
+  return http.requestPost(apiUrl + '/cms/page/add', params)
+}
+
+//页面查询
+export const page_get = id =>{
+  return http.requestGet(apiUrl + "/cms/page/get/" + id)
+}
+
+//頁面修改，採用put方式提交
+export const page_edit = (id, params) => {
+  return http.requestPut(apiUrl + '/cms/page/edit/' + id, params)
+}
+
+//删除数据
+export const page_del = id => {
+  return http.requestDelete(apiUrl + '/cms/page/del/' + id)
 }
--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <el-button type="primary" v-on:click="query" size="small">查询</el-button>
+    <!--查询表单-->
+    <el-form :model="params">
+      <el-select v-model="params.siteId" placeholder="请选择站点">
+        <el-option
+          v-for="item in siteList"
+          :key="item.siteId"
+          :label="item.siteName"
+          :value="item.siteId">
+        </el-option>
+      </el-select>
+      页面别名：<el-input v-model="params.pageAliase" style="width: 100px"></el-input>
+      <el-button type="primary" v-on:click="query" size="small">查询</el-button>
+      <router-link class="mui-tab-item"
+                   :to="{path:'/cms/page/add',
+                    query:{
+                      page:this.params.page,
+                      siteId:this.params.siteId
+                    }
+                   }">
+        <el-button type="primary" size="small">新增页面</el-button>
+      </router-link>
+    </el-form>
     <el-table
       :data="list"
       stripe
@@ -36,6 +57,18 @@
         label="创建时间"
         width="180">
       </el-table-column>
+      <el-table-column label="操作" with="80">
+        <template slot-scope="page">
+          <el-button
+            size="small"type="text"
+            @click="edit(page.row.pageId)">编辑
+          </el-button>
+          <el-button
+            size="small"type="text"
+            @click="del(page.row.pageId)">删除
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <el-pagination
       layout="prev, pager, next"
@@ -52,17 +85,21 @@
   export default {
     data() {
       return {
+        siteList:[],
         list:[],
         total:50,
         params:{
+          siteId:'',
+          pageAliase:'',
           page:1,//页面
-          size:2//每页显示个数
+          size:5//每页显示个数
         }
       }
     },
     methods:{
       //分页查询
-      changePage:function () {
+      changePage(page) {
+        this.params.page = page;
         this.query()
       },
       query:function () {
@@ -71,7 +108,48 @@
           this.total=res.queryResult.total
           this.list=res.queryResult.list
         })
+      },
+      //修改
+      edit:function (pageId) {
+        this.$router.push({path:'/cms/page/edit/'+pageId, query:{
+            page:this.params.page,
+            siteId:this.params.siteId
+          }})
+      },
+      //删除数据
+      del:function(pageId) {
+        this.$confirm('确认删除此页面吗？', '提示', {}).then(() => {
+          cmsApi.page_del(pageId).then((res) =>{
+            console.log(res);
+            if(res.success){
+              this.$message.success('删除成功');
+              //查询页面
+              this.query();
+            }else{
+              this.$message.error('删除失败');
+            }
+          });
+        });
       }
+    },
+    created(){
+      this.params.page = Number.parseInt(this.$route.query.page || 1);
+      this.params.siteId = this.$route.query.siteId || '';
+    },
+    mounted(){
+      //默认查询页面
+      this.query()
+      //初始化站点列表
+      this.siteList=[
+        {
+          siteId:'5a751fab6abb5044e0d19ea1',
+          siteName:'门户主站'
+        },
+        {
+          siteId:'102',
+          siteName:'测试站'
+        }
+      ]
     }
   }
 </script>
